1508a73e1b072a31169cf06767362800d8bca4e7
Monica <monica.hernandez_ascencio@hochbahn.de>
2021-08-27 14:18:48 +0200

    configuration

---
+++ producer1/src/main/java/de/mosaic/kafka/training/producer1/KafkaProducerConfiguration.java
@@ -0,0 +1,30 @@
+package de.mosaic.kafka.training.producer1;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfiguration {
+
+    @Bean
+    public ProducerFactory<String,String> producerFactory(){
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"http://localhost:9092");
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configs);
+    }
+
+    @Bean
+    public KafkaTemplate<String,String> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
