1eb0dc077056cfe9b8639b29c389e400a69e814a
Monica <monica.hernandez_ascencio@hochbahn.de>
2021-08-27 13:58:59 +0200

    producer1

---
+++ producer1/src/main/java/de/mosaic/kafka/training/producer1/Producer1Application.java
@@ -0,0 +1,13 @@
+package de.mosaic.kafka.training.producer1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Producer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Producer1Application.class, args);
+	}
+
+}
---
+++ producer1/src/test/java/de/mosaic/kafka/training/producer1/Producer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mosaic.kafka.training.producer1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Producer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
