b0ce2ccef3c81ea3caeb0316a8f2781cc3d4f6f1
Martin Strube <martin.strube@hochbahn.de>
2021-08-27 15:17:05 +0200

    add Kafka Admin - new Topic - new Topic rest mapping

--- producer1/src/main/java/de/mosaic/kafka/training/producer1/KafkaProducerConfiguration.java
+++ producer1/src/main/java/de/mosaic/kafka/training/producer1/KafkaProducerConfiguration.java
@@ -1,5 +1,6 @@
 package de.mosaic.kafka.training.producer1;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
@@ -17,14 +18,26 @@ public class KafkaProducerConfiguration {
     @Bean
     public ProducerFactory<String,String> producerFactory(){
         Map<String, Object> configs = new HashMap<>();
-        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"http://localhost:9092");
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "http://localhost:9092");
         configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return new DefaultKafkaProducerFactory<>(configs);
     }
 
     @Bean
-    public KafkaTemplate<String,String> kafkaTemplate(){
+    public KafkaTemplate<String, String> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
+    }
+
+//    @Bean
+//    public KafkaAdmin getKafkaAdmin() {
+//        Map<String, Object> stringObjectMap = new HashMap<>();
+//        stringObjectMap.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "http://localhost:9092");
+//        return new KafkaAdmin(stringObjectMap);
+//    }
+
+    @Bean
+    public NewTopic createNewTop() {
+        return new NewTopic("Topic-New", 2, (short) 1);
     }
 }
--- producer1/src/main/java/de/mosaic/kafka/training/producer1/Producer.java
+++ producer1/src/main/java/de/mosaic/kafka/training/producer1/Producer.java
@@ -8,15 +8,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController("/")
 public class Producer {
 
-    private KafkaTemplate<String,String> kafkaTemplate;
+    private KafkaTemplate<String, String> kafkaTemplate;
 
     public Producer(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    @RequestMapping(method = RequestMethod.GET,path = "/create-event")
-    public void createEvent(){
-        kafkaTemplate.send("Topic1","key-1","value-1");
+    @RequestMapping(method = RequestMethod.GET, path = "/create-event")
+    public void createEvent() {
+        kafkaTemplate.send("Topic1", String.valueOf(System.currentTimeMillis()), "value-1");
+        System.out.println("Event has been produced");
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/create-event-new")
+    public void createEventTopicNew() {
+        kafkaTemplate.send("Topic-New", String.valueOf(System.currentTimeMillis()), "value-new");
         System.out.println("Event has been produced");
     }
 }
