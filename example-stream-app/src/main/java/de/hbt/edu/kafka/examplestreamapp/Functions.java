50742a1e4c21425ef3fa7a3a375a66985c06061a
Klaus Richarz <kr@hbt.de>
2021-08-27 13:14:43 +0200

    remove connect container

--- example-stream-app/src/main/java/de/hbt/edu/kafka/examplestreamapp/Functions.java
+++ example-stream-app/src/main/java/de/hbt/edu/kafka/examplestreamapp/Functions.java
@@ -1,11 +1,11 @@
 package de.hbt.edu.kafka.examplestreamapp;
 
-import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 @Configuration
@@ -17,6 +17,15 @@ public class Functions {
             System.out.println(key + ", " + value);
             return new KeyValue<>(key, "new-value");
         });
+    }
+
+    @Bean
+    public Consumer<KStream<String, String>> salesData() {
+        return message -> {
+            message.foreach((key, value) -> {
+                System.out.println(key + " -> " + value);
+            });
+        };
     }
 
 }
